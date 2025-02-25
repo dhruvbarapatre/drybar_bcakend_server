51c77bd2c5c38d0213bb0faf95c2974c4b31985a
dhruvbarapatre <barapatredhruv280126@gmail.com>
2025-02-25 09:27:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const cors=require("cors")
 const app = express()
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://drybarclonedhruv.vercel.app",
     credentials:true
 }))
 app.use(cookieParser()) 
